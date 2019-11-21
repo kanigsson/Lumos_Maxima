90ad8ecea2b17bbf855de449ff466148dec3b2c9
Johannes Kanig <kanig@adacore.com>
2019-11-21 18:39:48 +0900

    tmp

--- src/email.ads
+++ src/email.ads
@@ -1,7 +1,16 @@
-package Email is
+package Email with SPARK_Mode is
 
-   type Email_Address_Type is new Integer; -- ???
+   type Email_Address_Type is private;
 
-   No_Email : constant Email_Address_Type := 0;
+   function Mk_Email (S : String) return Email_Address_Type;
+   function Get_Email_String (X : Email_Address_Type) return String;
+
+   No_Email : constant Email_Address_Type;
+
+private
+
+  type Email_Address_Type is new Natural;
+
+  No_Email : constant Email_Address_Type := 0;
 
 end Email;
